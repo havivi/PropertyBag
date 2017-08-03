723769495e79ea769c31cab690d439fbbe70b04f
Alon Havivi <havivi@gmail.com>
2017-08-03 11:36:49 +0200

    Description

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This app, by [Alon Havivi](http://www.e-office.com) ([@alonhavivi](http://www.twitter.com/alonhavivi))
 
-Icons made by [Freepik](http://www.freepik.com)  is licensed by CC 3.0 BY
+ ![alt text](https://4.bp.blogspot.com/-3SHmbaShOas/WYLtjs-D3cI/AAAAAAAAb1k/Xt-tVg8XIFUelzsnFjtbBV54LniOQXlqgCLcBGAs/s1600/propertybag.png)
--- pkg/Debug/LearningPathManager/PropertyBag_AppMain_Feature/App/services/datacontext.angular.js
+++ pkg/Debug/LearningPathManager/PropertyBag_AppMain_Feature/App/services/datacontext.angular.js
@@ -20,7 +20,9 @@
             getSubsites: getSubsites,
             getLists: getLists,
             getProperties: getProperties,
-            addPropertyBagWeb: addPropertyBagWeb 
+            addPropertyBagWeb: addPropertyBagWeb,
+            addPropertyBagList: addPropertyBagList,
+            deletePropertyBagWeb: deletePropertyBagWeb
         };
 
         // init service
@@ -29,14 +31,15 @@
         }
 
         function getSitesResource() {
-
+            var host = spContext.hostWeb.url.split('/');
+           
             return $resource("_api/search/query",
            {},
            {
                get: {
                    method: 'GET',
                    params: {
-                       'querytext': '\'* AND contentclass:sts_site\'',
+                       'querytext': '\'* AND Path:' +  location.protocol + '//' + host[2] + '* AND contentclass:sts_site\'',
                        'selectproperties': '\'Title,Path\'',
                        'rowlimit': 2000
                    },
@@ -156,11 +159,7 @@
 
             var deferred = $q.defer();
             resource.get({}, function (data) {
-                if (list) {
-                    deferred.resolve(data.d.properties);
-                } else {
-                    deferred.resolve(data.d);
-                }
+                deferred.resolve(data.d);
                 common.logger.log("retrieved app content", data, serviceId);
             }, function (error) {
                 deferred.reject(error);
@@ -172,10 +171,13 @@
 
         var web;
         var webProps;
+        var listProps;
         var context;
         var appContextSite;
+        var list;
+        var rootFolder;
 
-        function addPropertyBagWeb(site, key, value, indexed) {
+        function addPropertyBagWeb(site, key, value, indexed, indexedKeysExists, indexedKeysValue) {
 
             var deferred = $q.defer();
             context = new SP.ClientContext(spContext.hostWeb.appWebUrl);
@@ -185,15 +187,27 @@
             webProps = web.get_allProperties();
             context.load(web);
             context.load(webProps);
+            webProps.set_item(key, value)
+
+            if (indexed) {
+                var encodeValue = encodePropertyValue(value);
+                if (!indexedKeysExists) {
+                    webProps.set_item("vti_indexedpropertykeys", encodeValue);
+                } else {
+                    webProps.set_item("vti_indexedpropertykeys", indexedKeysValue + encodeValue + "|");
+                }
+            }
+            web.update();
+
+
             context.executeQueryAsync(
                 function () {
                     deferred.resolve();
-                    addNewProperty(key, value, indexed);
-                     
-                    
+
+
                 },
                 function (sender, args) {
-                    common.logger.logError(args.get_message(), error, serviceId);
+                    common.logger.logError(args.get_message(), args.get_message(), serviceId);
                     deferred.reject(args.get_message());
                 }
             );
@@ -201,58 +215,83 @@
             return deferred.promise;
         }
 
-        function addNewProperty(key, value, indexed) {
+        function addPropertyBagList(listName, site, key, value, indexed, indexedKeysExists, indexedKeysValue) {
+
             var deferred = $q.defer();
-            // Adds a new property or modified the value of an existing property. 
-            webProps.set_item(key, value)
-            // Check if "vti_indexedpropertykeys" property exists in the property bag. If yes, set the flag
+            context = new SP.ClientContext(spContext.hostWeb.appWebUrl);
+            appContextSite = new SP.AppContextSite(context, site);           
+            web = appContextSite.get_web();
+            list = web.get_lists().getByTitle(listName);           
+            rootFolder = list.get_rootFolder();            
+            listProps = rootFolder.get_listItemAllFields();
+            context.load(appContextSite.get_web());
+            context.load(web);
+            context.load(list);
+            context.load(rootFolder);
+            context.load(listProps);
+            listProps.set_item(key, value)
 
-            //var found = $.map($scope.properties, function (val) {
-            //    if (val.Key == 'vti_indexedpropertykeys') {
-
-            //        property.indexedKeysExists = true;
-            //    }
-            //});
-            //if (property.indexed === true) {
-            //    var encodedPropKey = EncodePropertyKey(property.key);
-
-            //    if (property.indexedKeysExists) {
-            //        var indexedProperties = webProps.get_item("vti_indexedpropertykeys");
-            //        if (indexedProperties.indexOf(encodedPropKey) > -1) {
-            //            // Means the property is already indexed
-
-            //            alert('The propertybag  "' + property.key + '" is already indexed.');
-            //        }
-            //        else {
-            //            // Add the encoded value to the property bag vti_indexedpropertykeys
-            //            var addEncodedKey = indexedProperties + encodedPropKey + "|"
-            //            webProps.set_item("vti_indexedpropertykeys", addEncodedKey);
-            //            // alert('The vti_indexedpropertykeys  "' + property.key + '" was added');
-            //        }
-            //    }
-            //    else {
-            //        // vti_indexedpropertykeys does not exist. Add the new property.
-            //        webProps.set_item("vti_indexedpropertykeys", encodedPropKey);
-            //    }
-            //}
-            web.update();
-            context.executeQueryAsync(
-            function () {
-                deferred.resolve();
-                //alert('The propertybag  "' + property.key + '" was added');
-                 
-            },
-            function (sender, args) {
-                //alert('Failed in adding propertybag  "' + property.key + '". Error: ' + args.get_message()); 
-
+            if (indexed) {
+                var encodeValue = encodePropertyValue(value);
+                if (!indexedKeysExists) {
+                    listProps.set_item("vti_indexedpropertykeys", encodeValue);
+                } else {
+                    listProps.set_item("vti_indexedpropertykeys", indexedKeysValue + encodeValue + "|");
+                }
             }
-        );
+            rootFolder.update();
+            list.update();
+            rootFolder.update();
+            context.executeQueryAsync(
+               function () {
+                   deferred.resolve();
+
+
+               },
+               function (sender, args) {
+                   common.logger.logError(args.get_message(), args.get_message(), serviceId);
+                   deferred.reject(args.get_message());
+               }
+           );
+
+            return deferred.promise;
         }
 
-        function EncodePropertyKey(propKey) {
+
+
+        function deletePropertyBagWeb(site, key) {
+
+            var deferred = $q.defer();
+            context = new SP.ClientContext(spContext.hostWeb.appWebUrl);
+            appContextSite = new SP.AppContextSite(context, site);
+            context.load(appContextSite.get_web());
+            web = appContextSite.get_web();
+            webProps = web.get_allProperties();
+            context.load(web);
+            context.load(webProps);
+            webProps.set_item(key);
+            web.update();
+            context.executeQueryAsync(
+                function () {
+                    deferred.resolve();
+                },
+                function (sender, args) {
+                    common.logger.logError(args.get_message(), args.get_message(), serviceId);
+                    deferred.reject(args.get_message());
+                }
+            );
+
+            return deferred.promise;
+        }
+
+
+
+
+
+        function encodePropertyValue(value) {
             var bytes = [];
-            for (var i = 0; i < propKey.length; ++i) {
-                bytes.push(propKey.charCodeAt(i));
+            for (var i = 0; i < value.length; ++i) {
+                bytes.push(value.charCodeAt(i));
                 bytes.push(0);
             }
             var b64encoded = window.btoa(String.fromCharCode.apply(null, bytes));
